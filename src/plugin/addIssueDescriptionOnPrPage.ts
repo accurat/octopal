bb02b8d2f2cbf595ad1f71dab2b7e6a8fa455d0e
Luca Falasco <luca.falasco@accurat.it>
2019-05-30 15:12:20 +0200

    finish up addIssueDescriptionOnPrPage

--- src/plugin/addIssueDescriptionOnPrPage.ts
+++ src/plugin/addIssueDescriptionOnPrPage.ts
@@ -1,62 +1,63 @@
-import * as $ from "jquery";
-import * as marked from "marked";
+import * as $ from 'jquery'
+import * as marked from 'marked'
 
-$(document).on("click", ".gwfp-issue-description-header", () => {
-  $(".gwfp-issue-description").toggleClass("visible");
-});
+$(document).on('click', '.gwfp-issue-description-header', () => {
+  $('.gwfp-issue-description').toggleClass('visible')
+})
 
 const appendIssueDescriptionBox = ({
   associatedIssueNumber,
   owner,
-  repo
+  repo,
 }: {
-  associatedIssueNumber: string;
-  owner: string;
-  repo: string;
+  associatedIssueNumber: string
+  owner: string
+  repo: string
 }) => {
   const issueDescriptionBox = $(`
-    <div class='gwfp-issue-description'>
+    <div class='gwfp-issue-description issue-${associatedIssueNumber}'>
       <div class='gwfp-issue-description-header'>
         ISSUE DESCRIPTION <a href="/${owner}/${repo}/issues/${associatedIssueNumber}">#${associatedIssueNumber}</a>
         <span class='gwfp-issue-description-arrow-down'>▼</span>
         <span class='gwfp-issue-description-arrow-up'>▲</span>
       </div>
-      <div class='gwfp-issue-description-content markdown-body markdown-format'>
+      <div class='gwfp-issue-description-content issue-${associatedIssueNumber} markdown-body markdown-format'>
         -
       </div>
     </div>
-  `);
-  $(".gwfp-issue-description").remove();
-  $("#discussion_bucket").prepend(issueDescriptionBox);
-};
+  `)
+  $(`.gwfp-issue-description.issue-${associatedIssueNumber}`).remove()
+  $('#discussion_bucket').prepend(issueDescriptionBox)
+}
 
 interface Issue {
-  body: string;
+  body: string
+  number: number
 }
 
 const appendIssueDescription = (issue: Issue) => {
-  const html = marked(issue.body);
-  $(".gwfp-issue-description-content").html(html);
-};
+  const html = marked(issue.body)
+  $(`.gwfp-issue-description-content.issue-${issue.number}`).html(html)
+}
 
 export default function({ owner, repo }: { owner: string; repo: string }) {
-  chrome.storage.sync.get({ githubToken: "" }, ({ githubToken }) => {
-    const apiBaseUrl = "https://api.github.com/";
-    const prTitle = $(".js-issue-title").text();
-    const getIssueNumberRE = /.*\(closes\s#(\d+)\).*/;
-    const [, associatedIssueNumber = undefined] =
-      prTitle.match(getIssueNumberRE) || [];
-    const apiToken = githubToken as string | undefined;
-    if (apiToken && associatedIssueNumber) {
-      appendIssueDescriptionBox({
-        associatedIssueNumber,
-        owner,
-        repo
-      });
-      $.get(
-        `${apiBaseUrl}repos/${owner}/${repo}/issues/${associatedIssueNumber}?access_token=${apiToken}`,
-        appendIssueDescription
-      );
-    }
-  });
+  chrome.storage.sync.get({ githubToken: '' }, ({ githubToken }) => {
+    const apiBaseUrl = 'https://api.github.com/'
+    const issues = $('.comment-body .issue-link')
+    Array.from(issues).forEach((issue: HTMLElement) => {
+      const associatedIssueNumber = issue.innerHTML.slice(1)
+      const apiToken = githubToken as string | undefined
+      if (apiToken && associatedIssueNumber) {
+        appendIssueDescriptionBox({
+          associatedIssueNumber,
+          owner,
+          repo,
+        })
+        $.get(
+          `${apiBaseUrl}repos/${owner}/${repo}/issues/${associatedIssueNumber}?access_token=${apiToken}`,
+          appendIssueDescription,
+        )
+      }
+    })
+  })
 }
